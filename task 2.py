3b8b380e0d194d8807d1800783db22ee4f57e149
MuhammadSaadBinJaved <145525146+MuhammadSaadBinJaved@users.noreply.github.com>
2023-09-20 10:11:17 +0500

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,40 @@
+print('"we are learning", he said')
+
+print('he said, "I\'m learning".')
+
+# TASK 2
+radius = 3.12
+pi = 3.14
+
+print(pi * radius ** 2)
+
+#task 3
+sap_id = input("write your sap id:")
+name = input("write your name:")
+
+print(name, sap_id)
+
+
+
+
+
+
+
+
+#lab3 
+#task1
+
+
+L = [4, 5, 1, 2, 9, 7, 10, 8]
+ 
+
+count = 0
+
+for i in L:
+    count += i
+ 
+
+avg = count/len(L)
+ 
+print("sum = ", count)
+print("average = ", avg)
